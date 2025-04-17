403e19263b5caf08a638d4e194051d4186604a17
sulthann1 <sulthanaawaliya155@gmail.com>
2025-04-17 18:32:27 +0700

    mengatur low and high

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -5,3 +5,12 @@ int arr [20], B[20]; //array utama dan array bantu
 int n;
 
 void mergeSort (int low, int high);
+{
+    if (low >= high)
+{ //step 1
+    return // step 1a
+}
+int mid = {low + high} /2;
+
+mergeSort
+}
