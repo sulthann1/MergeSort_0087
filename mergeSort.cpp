f96707f780cc7e6a51578a07491a5f75b4675b87
sulthann1 <sulthanaawaliya155@gmail.com>
2025-04-17 18:41:52 +0700

    mengatur jika i dan j pada array

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -4,13 +4,12 @@ using namespace std;
 int arr [20], B[20]; //array utama dan array bantu
 int n;
 
-void mergeSort (int low, int high);
-{
+void mergeSort (int low, int high) {
     if (low >= high)
 { //step 1
     return // step 1a
 }
-int mid = {low + high} /2;
+int mid = {low + high} //2;
 
 mergeSort {low,mid};
 mergeSort {mid+1, high};
@@ -19,6 +18,20 @@ int i = low;
 int j, k;
 j = mid +1;
 k = low;
+
+while (i <= mid && j <= high)
+{
+    if (arr[i]<= arr[j])
+    {
+    B[k] = arr [i];
+    i++;
+    }
+else
+{
+    B[k] = arr[j];
+    j++;
+} 
+}
 
 
 }
